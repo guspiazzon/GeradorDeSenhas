07014b933ea91f7a1f28f75b90b42d69da3e3c57
Gustavo Piazzon Torres <gustavopiazzont@gmail.com>
2022-06-05 12:40:41 -0300

    Criação de uma aplicação em Python para gerar senhas aleatórias

---
+++ gs.py
@@ -0,0 +1,10 @@
+import random, string
+
+tamanho = int(input('Digite o tamanho da senha que você deseja: '))
+chars = string.ascii_letters + string.digits + '!@#$%()*-/+.;:?º{]}[=_'
+rnd = random.SystemRandom()
+
+print('' .join(rnd.choice(chars) for i in range(tamanho)))
+
+
+
